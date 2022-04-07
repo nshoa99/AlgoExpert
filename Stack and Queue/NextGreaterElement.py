25f00808de291e45718617b520280cfeab9ee066
= <=>
2022-04-08 05:16:34 +0700

    Leetcode: Next Greater Element I

---
+++ Stack and Queue/NextGreaterElement.py
@@ -0,0 +1,17 @@
+# Leetcode: https://leetcode.com/problems/next-greater-element-i/
+#
+
+def nextGreaterElement(nums1, nums2):
+    result = [-1] * len(nums1)
+    mapping = {nums1[i]: i for i in range(len(nums1))}
+    stack = []
+    i = 0
+    while i < len(nums2):
+        if stack or nums2[i] < stack[-1]:
+            stack.append(nums2[i])
+            i += 1
+        else:
+            if stack[-1] in mapping:
+                result[mapping[stack[-1]]] = nums2[i]
+            stack.pop()
+    return result
