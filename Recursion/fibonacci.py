a34e951e0625b55acdf1b1e293b453b67b9f5e45
= <=>
2022-04-13 07:18:06 +0700

    Leetcode | Fibonacci

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -7,5 +7,16 @@ def fibo(n):
 
     return fibo(n-1) + fibo(n-2)
 
+# Dynamic programming
 
-print(fibo(5))
+def fibo_dynamic(n):
+    arr = [0] * n
+    arr[0] = 1
+    arr[1] = 2
+    for i in range(2, n):
+        arr[i] = arr[i - 1] + arr[i -2]
+
+    return arr[-1]
+
+
+print(fibo_dynamic(5))
