7d88e2c3355cf3c51b0d3a20fad3da66fdaac0b6
= <=>
2022-04-18 04:26:19 +0700

    Bubble Sort

---
+++ Sort/BubbleSort.py
@@ -0,0 +1,11 @@
+# https://www.algoexpert.io/questions/Bubble%20Sort
+# Average: Time complexity O(n^2) | Space complexity O(1)
+
+def bubbleSort(array):
+    for i in range(len(array)):
+        for j in range(i, len(array)):
+            if array[i] > array[j]:
+                array[i], array[j] = array[j], array[i]
+    return array
+
+print(bubbleSort([8, 5, 2, 9, 5, 6, 3]))
